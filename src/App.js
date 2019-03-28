c9c8a7d4d1f110ddf6415cae65579911ba2e2255
Dusan Milic <milicdusan89@gmail.com>
2019-03-28 14:47:05 +0100

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Profile from "./Components/Profile";
+import Nav from "./Components/Nav";
 
 class App extends Component {
   render() {
     return (
       <>
+        <Nav />
         <Route path="/" exact component={Home} />
         <Route path="/profile" component={Profile} />
       </>
