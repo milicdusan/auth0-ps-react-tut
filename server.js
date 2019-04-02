cff49fdbe5c5e4979fbb2351acff7a7d8b427c2a
Dusan Milic <milicdusan89@gmail.com>
2019-04-02 21:03:26 +0200

    Typo fix

--- server.js
+++ server.js
@@ -4,18 +4,22 @@ const jwt = require("express-jwt");
 const jwksRsa = require("jwks-rsa");
 
 const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWKS endpoint.
   secret: jwksRsa.expressJwtSecret({
-    cache: true,
+    cache: true, // cache the signing key
     rateLimit: true,
-    jwksRequestsPerMinute: 5,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute
     jwksUri: `https://${
-      process.env.REACTU_APP_AUTH0_DOMAIN
+      process.env.REACT_APP_AUTH0_DOMAIN
     }/.well-known/jwks.json`
   }),
 
-  audience: process.env.REACTU_APP_AUTH0_AUDIENCE,
-  issuer: `https://${process.env.REACTU_APP_AUTH0_DOMAIN}`,
+  // Validate the audience and the issuer.
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
 
+  // This must match the algorithm selected in the Auth0 dashboard under your app's advanced settings under the OAuth tab
   algorithms: ["RS256"]
 });
 
