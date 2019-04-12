ac3397981be916afad9221869edfcae48bea1d3a
Dusan Milic <dusan.milic@shareit.rs>
2019-04-12 12:09:29 +0200

    Redirect to previous page after login

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,5 +1,7 @@
 import auth0 from "auth0-js";
 
+const REDIRECT_ON_LOGIN = "redirect_on_login";
+
 export default class Auth {
   constructor(history) {
     this.history = history;
@@ -17,19 +19,29 @@ export default class Auth {
   }
 
   login = () => {
+    localStorage.setItem(
+      REDIRECT_ON_LOGIN,
+      JSON.stringify(this.history.location)
+    );
     this.auth0.authorize();
   };
 
   handleAuthentication = () => {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
+        debugger;
         this.setSession(authResult);
-        this.history.push("/");
+        const redirectLocation =
+          localStorage.getItem(REDIRECT_ON_LOGIN) === "undefined"
+            ? "/"
+            : JSON.parse(localStorage.getItem(REDIRECT_ON_LOGIN));
+        this.history.push(redirectLocation);
       } else if (err) {
         this.history.push("/");
         alert(`Error: ${err.error}. Check the console for more details.`);
         console.log(err);
       }
+      localStorage.removeItem(REDIRECT_ON_LOGIN);
     });
   };
 
